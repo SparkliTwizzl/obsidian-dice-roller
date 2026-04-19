3fe67ca9eb50adc86c2d20ff487fd431dccf0239
SparkliTwizzl <github@sparklitwizzl.com>
2026-04-18 22:56:54 -0600

    Update lexer tests to include new behavior

--- test/parser/lexer.test.ts
+++ test/parser/lexer.test.ts
@@ -547,23 +547,43 @@ test('Lexer should parse "6dF"', () => {
     ]);
 });
 
+// ALIASED ROLLS ==============================================================
+
+test('Lexer should parse "1d6 @\"rollAlias\""', () => {
+    let actual = Lexer.parse("1d6 @\"rollAlias\"").unwrap().map(toLexicalToken);
+    expect(actual).toEqual([
+        {
+            conditions: undefined,
+            parenedDice: undefined,
+            type: "dice",
+            value: "1d6"
+        },
+        {
+            conditions: undefined,
+            parenedDice: undefined,
+            type: "alias",
+            value: "rollAlias"
+        }
+    ]);
+});
+
 // CHAINED ROLLS ==============================================================
 
-test('Lexer should not parse "1d6; 1d4 * (1d2 - 1); ~\"|\"" when ChainRoller is disabled', () => {
+test('Lexer should not parse "1d6; 1d4 * (1d2 - 1); ~\"|\" @\"rollAlias\"" when ChainRoller is disabled', () => {
     let _prev = Lexer.isChainRollerEnabled;
     Lexer.setEnableChainRoller(false);
 
-    let actual = Lexer.parse("1d6; 1d4 * (1d2 - 1); ~\"|\"").unwrapErr();
+    let actual = Lexer.parse("1d6; 1d4 * (1d2 - 1); ~\"|\" @\"rollAlias\"").unwrapErr();
     expect(actual).toEqual("Could not parse");
 
     Lexer.setEnableChainRoller(_prev);
 });
 
-test('Lexer should parse "1d6; 1d4 * (1d2 - 1); ~\"|\"" when ChainRoller is enabled', () => {
+test('Lexer should parse "1d6; 1d4 * (1d2 - 1); ~\"|\" @\"rollAlias\"" when ChainRoller is enabled', () => {
     let _prev = Lexer.isChainRollerEnabled;
     Lexer.setEnableChainRoller(true);
 
-    let actual = Lexer.parse("1d6; 1d4 * (1d2 - 1); ~\"|\"").unwrap().map(toLexicalToken);
+    let actual = Lexer.parse("1d6; 1d4 * (1d2 - 1); ~\"|\" @\"rollAlias\"").unwrap().map(toLexicalToken);
     expect(actual).toEqual([
         {
             conditions: undefined,
@@ -582,6 +602,12 @@ test('Lexer should parse "1d6; 1d4 * (1d2 - 1); ~\"|\"" when ChainRoller is enab
             parenedDice: undefined,
             type: "resultSeparator",
             value: "|"
+        },
+        {
+            conditions: undefined,
+            parenedDice: undefined,
+            type: "alias",
+            value: "rollAlias"
         }
     ]);
 
